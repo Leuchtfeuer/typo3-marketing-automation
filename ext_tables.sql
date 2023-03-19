c0f9e24a823b832fb87cc96101e9723e8e671421
Fabian Martin <f.martin@leuchtfeuer.com>
2023-03-19 08:18:14 +0100

    [BUGFIX] Add tx_marketingautomation_persona column to pages and tt_content tables

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,18 @@
 #
 # Table structure for table 'tx_marketingautomation_persona'
 #
-CREATE TABLE tx_marketingautomation_persona (
-    title varchar(255) DEFAULT '' NOT NULL,
+CREATE TABLE tx_marketingautomation_persona
+(
+    title       varchar(255) DEFAULT '' NOT NULL,
     description text
+);
+
+CREATE TABLE pages
+(
+    tx_marketingautomation_persona varchar(100) DEFAULT '' NOT NULL,
+);
+
+CREATE TABLE tt_content
+(
+    tx_marketingautomation_persona varchar(100) DEFAULT '' NOT NULL,
 );
