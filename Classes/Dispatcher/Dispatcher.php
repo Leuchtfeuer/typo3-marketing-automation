103c69e125cbe736734bfc8911c349827492dab8
Niklas Grieger <n.grieger@leuchtfeuer.com>
2026-04-23 09:52:13 +0200

    [TASK] Remove duplicated comment [TER-435] [TER-440]

--- Classes/Dispatcher/Dispatcher.php
+++ Classes/Dispatcher/Dispatcher.php
@@ -13,15 +13,6 @@ declare(strict_types=1);
 
 namespace Leuchtfeuer\MarketingAutomation\Dispatcher;
 
-/*
- * This file is part of the "Marketing Automation" extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * Team Yoda <dev@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
- */
-
 use Leuchtfeuer\MarketingAutomation\Persona\Persona;
 use Leuchtfeuer\MarketingAutomation\Storage\Cookie;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
